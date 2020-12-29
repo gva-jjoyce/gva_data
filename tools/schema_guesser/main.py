cf6fa9d29bfc8dd016c350c4b3edefa5f4db2d91
Justin Joyce <justin.joyce@lloydsbanking.com>
2020-12-29 13:11:45 +0000

    schema_guesser

---
+++ tools/schema_guesser/main.py
@@ -0,0 +1,33 @@
+"""
+Schema Guesser
+
+Reads through a dataset to 'guess' the schema.
+
+Current implementation only lists all the values in a set of fields to
+work out the set of symbols for an enumerated type.
+"""
+from gva.data import Reader
+from gva.data.formats import dictset
+import json
+
+reader = Reader(
+        project='dcsgva-da-prd',
+        from_path='dcsgva-da-prd-ai-notebook/02_INTERMEDIATE/VIEWS/NVD_CVE_SUMMARY/%datefolders/')
+
+values = {}
+
+for record in reader:
+    
+    for k,v in record.items():
+        
+        if k not in ['CVE', 'CWE', 'publishedDate', 'Description', 'v2.0:vectorString', 'v2.0:baseScore', 'v2.0:exploitabilityScore', 'v2.0:impactScore', 'v3.0:vectorString', 'v3.0:baseScore', 'v3.0:exploitabilityScore', 'v3.0:impactScore']:
+    
+            e = values.get(k, [])
+            if not v in e:
+                e.append(v)
+                values[k] = e
+    
+schema = []
+for k, v in values.items():
+    row = { 'name': k, 'type': 'enum', 'symbols': v }
+    schema.append(row)
