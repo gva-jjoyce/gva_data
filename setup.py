534f6e2992fcfecd97721b92ed40eca958787dcf
Justin Joyce <justin.joyce@lloydsbanking.com>
2020-12-30 16:34:45 +0000

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
    name='gva.data',
-   version='0.1.23',
+   version='0.1.24',
    description='GVA Data Libraries',
    long_description=long_description,
    long_description_content_type="text/markdown",
